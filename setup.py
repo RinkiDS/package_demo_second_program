6d74af4b9a188c131fc8e014f1fc1eea5a9d84c7
Rinki <rinki.sharma@gmail.com>
2021-09-30 15:37:53 +0530

    message

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-PROJECT_NAME ="package_demo"
+PROJECT_NAME ="package_demo1"
 USER_NAME ="RinkiDS"
 
 setuptools.setup(
